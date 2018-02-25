99604362c14fd980c749de208cc17d0af6b554fa
IOT_lab <jeffsan@gmx.com>
2018-02-25 13:56:40 +0800

    update license

--- modules/kervolution.py
+++ modules/kervolution.py
@@ -1,6 +1,21 @@
 # -* coding=utf-8 *-
-# Kernel Convolution Networks
-# Stride left
+# Kervolution Neural Networks
+# This file is part of the project of Kervolutional Neural Networks
+# It implements the kervolution for 4D tensors in Pytorch
+# Copyright (C) 2018 [Wang, Chen] <wang.chen@zoho.com>
+# Nanyang Technological University (NTU), Singapore.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import torch
 import torch.nn as nn
