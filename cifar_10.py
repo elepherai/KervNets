42a3b1730ccbfcf8c0947bffbb87602ae5ca4e8c
Wang Chen <jeffsan@gmx.com>
2018-03-14 20:15:31 +0800

    fix import

--- cifar_10.py
+++ cifar_10.py
@@ -13,8 +13,7 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
-from models import *
-# from utils import progress_bar
+from models.kresnet import *
 from torch.autograd import Variable
 
 
